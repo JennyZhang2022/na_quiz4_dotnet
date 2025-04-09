a2f920a28500575dfd650bf03ca2aa31aaff7a1a
JennyZhang2022 <jenny.zhang3535@outlook.com>
2025-04-09 12:11:10 -0400

    Initial commit to the repo

---
+++ na_quiz4_dotnet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace na_quiz4_dotnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
